8c85079065b8d8900133a0f83a1fe98c1abb5176
hieusnguyen0709 <hieusnguyen0709@gmail.com>
2025-09-22 15:33:11 +0700

    update white list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-    // 'http://localhost:5173'
+    'https://trello-web-nine-flax.vercel.app'
 ]
 
 export const BOARD_TYPES = {
